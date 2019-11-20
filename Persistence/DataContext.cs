564fb0feaa799b21ecd90cc4eca15031f02fa3f2
Max Magyar <magyarmr@gmatc.matc.edu>
2019-11-20 11:07:51 -0600

    DataContext.cs

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,13 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext: DbContext
+    {
+        public DbSet<Value> Values { get; set; }
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
